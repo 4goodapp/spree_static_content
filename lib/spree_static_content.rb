84e1bb9b5bed05934d33160ea5c9e04de3ab144d
4goodapp <4goodapp@gmail.com>
2020-10-08 22:19:15 +0000

    Attempt to make it work with multi langue 3

--- lib/spree_static_content.rb
+++ lib/spree_static_content.rb
@@ -17,8 +17,12 @@ end
 module Spree
   class StaticPage
     def self.matches?(request)
-      return false if request.path =~ %r{\A\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+}
-      !Spree::Page.find_by(slug: request.path).nil?
+ 
+      
+      path = request.path_parameters[:path]
+      return false if path =~ %r{\A+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+}
+      !self.finder_scope.find_by(slug: [path, "/#{path}"]).nil?
+      
     end
   end
 end
